1ba73bb4459de6dadbb09abf513e9390da3e5b97
jasper.5in <jasperliu.5in@gmail.com>
2026-04-23 14:23:12 +0800

    div

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,10 +1,26 @@
-let counterbutton = document.getElementById("counter-button");
+let counterbuttonAdd1 = document.getElementById("counter-button-add1");
+let counterbuttonMinus1 = document.getElementById("counter-button-minus1");
+let counterbuttonClear = document.getElementById("counter-button-clear");
 let counter = document.getElementById("counter");
 let number = 0;
 
-function ChangeNumberMode() {
+function Add1() {
     number = number + 1;
     counter.textContent = number;
 }
 
-counterbutton.addEventListener("click", ChangeNumberMode);
+function Minus1() {
+    number = number - 1;
+    counter.textContent = number;
+}
+
+function clear() {
+    number = 0
+    counter.textContent = number;
+}
+
+console.log(number)
+
+counterbuttonAdd1.addEventListener("click", Add1);
+counterbuttonMinus1.addEventListener("click", Minus1);
+counterbuttonClear.addEventListener("click", clear);
