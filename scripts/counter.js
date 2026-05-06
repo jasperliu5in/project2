de9a0d6cf7012d091b57442f248039b9cbd9abe1
jasper.5in <jasperliu.5in@gmail.com>
2026-05-06 15:47:23 +0800

    calculator final stage

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,45 +1,100 @@
 //Make Variables
 
-let counterbuttonAdd1 = document.getElementById("counter-button-add1");
-let counterbuttonAdd2 = document.getElementById("counter-button-add2");
-let counterbuttonMinus1 = document.getElementById("counter-button-minus1");
-let counterbuttonMinus2 = document.getElementById("counter-button-minus2");
-let one = document.getElementById("One")
-let two = document.getElementById("Two")
-let three = document.getElementById("Three")
-let four = document.getElementById("Four")
-let five = document.getElementById("Five")
-let six = document.getElementById("Six")
-let seven = document.getElementById("Seven")
-let eight = document.getElementById("Eight")
-let nine = document.getElementById("Nine")
-let zero = document.getElementById("Zero")
-let counterbuttonClear = document.getElementById("counter-button-clear");
-let Addbutton = document.getElementById("plus")
-let Minusbutton = document.getElementById("minus")
-let Equalbutton = document.getElementById("equal")
-let Oldnumber = 0
+let Add1button = document.getElementById("add1");
+let Minus1button = document.getElementById("minus1");
+let one = document.getElementById("One");
+let two = document.getElementById("Two");
+let three = document.getElementById("Three");
+let four = document.getElementById("Four");
+let five = document.getElementById("Five");
+let six = document.getElementById("Six");
+let seven = document.getElementById("Seven");
+let eight = document.getElementById("Eight");
+let nine = document.getElementById("Nine");
+let zero = document.getElementById("Zero");
+let Clear = document.getElementById("clear");
+let Addbutton = document.getElementById("plus");
+let Minusbutton = document.getElementById("minus");
+let Equalbutton = document.getElementById("equal");
+let Multiplybutton = document.getElementById("multiply");
+let Dividebutton = document.getElementById("divide")
 let counter = document.getElementById("counter");
+let dotbutton = document.getElementById("dot");
 let number = 0;
+let Oldnumber = 0;
+let symbol = 0;
+let dottrue = "no"
+let newone = 1
 
 //Define Functions
 function Show(num) {
     counter.textContent = num;
 }
 
-function Rember() {}
+  function plus(event){
+    Oldnumber = number;
+    number = 0;
+    Show(Oldnumber);
+    console.log(Oldnumber, number);
+    symbol = "Plus";
+}
 
-function Add() {}
+function minus(event){
+    Oldnumber = number;
+    number = 0;
+    Show(Oldnumber);
+    console.log(Oldnumber, number);
+    symbol = "Minus";
+}
 
-function Minus() {}
+function multiply(event){
+    Oldnumber = number;
+    number = 0;
+    Show(Oldnumber);
+    console.log(Oldnumber, number);
+    symbol = "multiply";
+}
+
+function divide(event){
+    Oldnumber = number;
+    number = 0;
+    Show(Oldnumber);
+    console.log(Oldnumber, number);
+    symbol = "divide";
+}
+
+function equal(event){
+   if(symbol==="Plus"){
+    answer = Oldnumber + number;
+    number = answer;
+    Oldnumber = 0;
+    Show(answer);
+};
+   if(symbol==="Minus"){
+    answer = Oldnumber - number;
+    number = answer;
+    Oldnumber = 0;
+    Show(answer);
+};
+   if(symbol==="multiply"){
+    answer = Oldnumber * number;
+    number = answer;
+    Oldnumber = 0;
+    Show(answer);
+};
+   if(symbol==="divide"){
+    answer = Oldnumber / number;
+    number = answer;
+    Oldnumber = 0;
+    Show(answer);
+};
+}
+function dotchange() {
+    dottrue = "yes";
+}
 
 function Add1() {
     number = number + 1;
-    Show(number);
-}
-
-function Add2() {
-    number = number + 2;
     Show(number);
 }
 
@@ -48,44 +103,39 @@ function Minus1() {
     Show(number);
 }
 
-function Minus2() {
-    number = number - 2;
-    Show(number)
-}
-
 function One() {
     number = number * 10 + 1;
-    Show(number)
+    Show(number);
 }
 
 function Two() {
     number = number * 10 + 2;
-    Show(number)
+    Show(number);
 }
 
 function Three() {
     number = number * 10 + 3;
-    Show(number)
+    Show(number);
 }
 
 function Four() {
     number = number * 10 + 4;
-    Show(number)
+    Show(number);
 }
 
 function Five() {
     number = number * 10 + 5;
-    Show(number)
+    Show(number);
 }
 
 function Six() {
     number = number * 10 + 6;
-    Show(number)
+    Show(number);
 }
 
 function Seven() {
     number = number * 10 + 7;
-    Show(number)
+    Show(number);
 }
 
 function Eight() {
@@ -95,24 +145,22 @@ function Eight() {
 
 function Nine() {
     number = number * 10 + 9;
-    Show(number)
+    Show(number);
 }
 
 function Zero() {
     number = number * 10 + 0;
-    Show(number)
+    Show(number);
 }
 function clear() {
     number = 0;
-    Show(number)
+    Show(number);
 }
 
 //Define Button Event Listeners
 
-counterbuttonAdd1.addEventListener("click", Add1);
-counterbuttonAdd2.addEventListener("click", Add2);
-counterbuttonMinus1.addEventListener("click", Minus1);
-counterbuttonMinus2.addEventListener("click", Minus2);
+Add1button.addEventListener("click", Add1);
+Minus1button.addEventListener("click", Minus1);
 one.addEventListener("click", One);
 two.addEventListener("click", Two);
 three.addEventListener("click", Three);
@@ -123,4 +171,10 @@ seven.addEventListener("click", Seven);
 eight.addEventListener("click", Eight);
 nine.addEventListener("click", Nine);
 zero.addEventListener("click", Zero);
-counterbuttonClear.addEventListener("click", clear);
+Clear.addEventListener("click", clear);
+Addbutton.addEventListener("click", plus);
+Minusbutton.addEventListener("click", minus);
+Multiplybutton.addEventListener("click", multiply);
+Dividebutton.addEventListener("click", divide);;
+Equalbutton.addEventListener("click", equal);
+dotbutton.addEventListener("click", dotchange);
